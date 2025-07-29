ab5ff918cb0469a464e4417110f429317cd40f47
rishgad <rishaangad@gmail.com>
2025-07-29 11:44:13 -0500

    Added back index.html

--- src/App.js
+++ src/App.js
@@ -56,6 +56,10 @@ export default function App() {
   }, []);
   
   useEffect(() => {
+    fetchInitialArticles();
+  }, [fetchInitialArticles]);
+  
+  useEffect(() => {
     localStorage.setItem('sentArticles', JSON.stringify(sentArticles));
   }, [sentArticles]);
 
